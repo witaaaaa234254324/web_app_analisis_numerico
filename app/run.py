b76dcd322d3184885d34be6b588ee6ab41e7e857
WITA <carloscallizaya369@gmail.com>
2025-12-08 23:27:04 -0400

    Update run.py

--- app/run.py
+++ app/run.py
@@ -43,9 +43,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_ENV") != "production"
     app.run(debug=debug, host='0.0.0.0', port=port)
-
-if __name__ == "__main__":
-    # Crea las tablas si no existen
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
